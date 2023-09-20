67c4cbe484d3efc96b97a5e6c955250b4ff29dd4
João Lucas Crescencio Granjeiro Barbosa <20221148060002@ifrn.local>
2023-09-20 08:12:26 -0300

    adicionando funcionalidades

--- vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/controller/CategoriaController.java
+++ vendas-estoque/src/main/java/br/edu/ifrn/vendasestoque/controller/CategoriaController.java
@@ -2,25 +2,35 @@ package br.edu.ifrn.vendasestoque.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.edu.ifrn.vendasestoque.domain.categoria.Categoria;
 import br.edu.ifrn.vendasestoque.repository.CategoriaRepository;
 
 @RestController
-@RequestMapping("categoriass")
+@RequestMapping("categorias")
 
 public class CategoriaController {
     
     @Autowired
     private CategoriaRepository repository;
 
+    @GetMapping
+    public String teste(){
+        return "Teste";
+    }
+
     @PostMapping
-    public ResponseEntity<Categoria> cadastrarCategoria(@RequestBody Categoria categoria){
-        return ResponseEntity.ok(repository.save(categoria));
+    public ResponseEntity cadastrarCategoria(@RequestBody Categoria categoria, UriComponentsBuilder uriComponentsBuilder){
+        Categoria categoriaLocal = repository.save(categoria);
+        var uri = uriComponentsBuilder.path("/categorias/{id}").buildAndExpand(categoriaLocal.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
         
     }
 }
